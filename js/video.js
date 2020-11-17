45b7c7906a1064c5f35b39023dc1776d91625b34
mainakhala <45867319+mainakhala@users.noreply.github.com>
2020-11-17 17:25:13 -0500

    Update video.js

--- js/video.js
+++ js/video.js
@@ -17,11 +17,25 @@ document.querySelector("#slower").addEventListener("click", function() {
 	video.playbackRate = 0.1;
 });
 
-document.querySelector("#slower").addEventListener("click", function() {
+document.querySelector("#faster").addEventListener("click", function() {
 	video.playbackRate = 1.1;
 });
 
 document.querySelector("#skip").addEventListener("click", function() {
 video.currentTime(video.currentTime() + 5);
-}
+});
 
+document.querySelector("#mute").addEventListener("click",function() {
+	if (video.muted ===  true);
+		video.muted = false;
+	else 
+		video.muted = true;
+});
+
+document.querySelector("#volumeSlider").addEventListener("change", function() {
+	video.volume = document.getElementById("#volumeSlider").value;
+	});
+
+document.querySelector("#old").addEventListener("click", function() {
+	video.filter.value = grayscale(100%);
+	});
