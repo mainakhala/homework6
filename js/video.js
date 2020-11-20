af9690081db3479c0c7db674e43b349721d2c035
mainakhala <45867319+mainakhala@users.noreply.github.com>
2020-11-20 10:14:35 -0500

    Update video.js

--- js/video.js
+++ js/video.js
@@ -7,18 +7,31 @@ window.addEventListener("load", function() {
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
 	video.play();
+	
+	document.getElementById("volumeSlider").addEventListener("change",function(){
+		video.volume = this.value / 100;
+	});
+	
 });
 
+
+
 document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
 	video.pause();
 });
 
 document.querySelector("#slower").addEventListener("click", function() {
 	video.playbackRate = 0.1;
+	console.log("video.playbackRate");
 });
+
+
+
 
 document.querySelector("#faster").addEventListener("click", function() {
 	video.playbackRate = 1.1;
+	console.log("video.playbackRate");
 });
 
 document.querySelector("#skip").addEventListener("click", function() {
@@ -28,8 +41,10 @@ video.currentTime(video.currentTime() + 5);
 document.querySelector("#mute").addEventListener("click",function() {
 	if (video.muted ===  true);
 		video.muted = false;
+		document.querySelector("#mute").value= "unmute";
 	else 
 		video.muted = true;
+		document.querySelector("#mute").value= "mute"
 });
 
 document.querySelector("#volumeSlider").addEventListener("change", function() {
@@ -37,5 +52,6 @@ document.querySelector("#volumeSlider").addEventListener("change", function() {
 	});
 
 document.querySelector("#old").addEventListener("click", function() {
-	video.filter.value = grayscale(100%);
+	// video.filter.value = grayscale(100%);
+	document.getElementById("old").id = ".oldTime";
 	});
